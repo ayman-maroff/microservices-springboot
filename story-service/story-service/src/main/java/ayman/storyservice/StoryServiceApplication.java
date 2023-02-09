0531fd0d915af310d0f87d971b90f241078094e5
ayman-maroff <aymnmrf@gmail.com>
2023-02-09 15:27:58 +0200

    7.convert services to Eureka Clients

--- story-service/story-service/src/main/java/ayman/storyservice/StoryServiceApplication.java
+++ story-service/story-service/src/main/java/ayman/storyservice/StoryServiceApplication.java
@@ -2,8 +2,9 @@ package ayman.storyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
+@EnableDiscoveryClient
 public class StoryServiceApplication {
 
 	public static void main(String[] args) {
