2b9769411f23182605cae5d55c8f471d77c42b49
ayman-maroff <aymnmrf@gmail.com>
2023-02-09 23:39:34 +0200

    11.calling external books api

--- story-service/story-service/src/main/java/ayman/storyservice/controllers/StoryController.java
+++ story-service/story-service/src/main/java/ayman/storyservice/controllers/StoryController.java
@@ -3,6 +3,7 @@ import ayman.storyservice.exception.StoryNotFoundException;
 import ayman.storyservice.models.Story;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +28,18 @@ public class StoryController {
         this.stories.add(new Story(4, "The Gift of the Magi", new Date(), 1));
         this.stories.add(new Story(5, "Rip Van Winkle", new Date(), 1));
     }
+    @Value("${api.key}")
+    private String apiKey;
+    @Autowired
+    RestTemplate restTemplate;
 
+    @GetMapping(path = "/book/kind/{kind}")
+
+    public String authorName(@PathVariable("kind") final String kind){
+        System.out.print(apiKey+kind);
+        String responseEntity = restTemplate.getForObject("https://www.googleapis.com/books/v1/volumes?q="+kind+"&key="+apiKey, String.class);
+        return responseEntity;
+    }
     @GetMapping(path = "/{id}")
     public Story findById(@PathVariable("id") Integer id) {
         log.info(String.format("stories.findById(%d)", id));
