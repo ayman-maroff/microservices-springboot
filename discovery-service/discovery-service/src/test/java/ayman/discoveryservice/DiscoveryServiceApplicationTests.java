42f24f97b37231ce768eb12bea6c45a3d3a71cbf
ayman-maroff <aymnmrf@gmail.com>
2023-02-09 15:05:27 +0200

    6.add discovery-service

---
+++ discovery-service/discovery-service/src/test/java/ayman/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ayman.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
